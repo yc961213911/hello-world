20bbadfed091b0d499fa7b3db3a597aa03003152
yc961213911 <yc3181@dingtalk.com>
2021-02-24 16:17:47 +0800

    channge

--- led.c
+++ led.c
@@ -27,13 +27,6 @@ void LED_Init(void)
  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		 //IO口速度为50MHz
  GPIO_Init(GPIOB, &GPIO_InitStructure);					 //根据设定参数初始化GPIOB.5
  GPIO_ResetBits(GPIOB,GPIO_Pin_3 | GPIO_Pin_4);						 //PB.5 输出高
-	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);//初始化
-	GPIO_SetBits(GPIOC,GPIO_Pin_0 | GPIO_Pin_1);//GPIOF9,F10设置高，灯灭
 
 }
  
